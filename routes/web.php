b11e035f8e697b7dc8c8e8fc10166251db46e9b4
Farhan Asif <f.asif2010@gmail.com>
2020-08-03 17:05:43 +0600

    laravel route fix

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/{path?}', function () {
+    return view('welcome');
+})->where('path', '[\/\w\.-]*');
+
+Auth::routes();
+//Route::get('/home', 'HomeController@index')->name('home');
