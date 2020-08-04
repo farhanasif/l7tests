34658be5e61d8b9c734a884d03bbb1d918944ac5
Farhan Asif <f.asif2010@gmail.com>
2020-08-04 23:59:12 +0600

    dummy auth design

--- resources/js/components/SignUp.js
+++ resources/js/components/SignUp.js
@@ -2,13 +2,29 @@ import React from 'react';
 
 function SignUp() {
     return (
-        <div className="container">
+        <div className="container mt-5">
             <div className="row justify-content-center">
-                <div className="col-md-8">
+                <div className="col-md-6">
                     <div className="card">
-                        <div className="card-header">SignUp</div>
+                        <div className="card-header">Register Account</div>
 
-                        <div className="card-body">I'm an SignUp component!</div>
+                        <div className="card-body">
+                        <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Full Name</label>
+                            <input type="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" />
+                        </div>
+                        <button type="submit" class="btn btn-primary">Register</button>
+                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/js/components/SingIn.js
+++ resources/js/components/SingIn.js
@@ -2,41 +2,28 @@ import React from 'react';
 
 function SignIn() {
     return (
-        <div className="container-login mt-5">
-            <div className="d-flex justify-content-center h-100">
-                <div className="card-login">
-                    <div className="card-header-login">
-                        <h3>Sign In</h3>
-                    </div>
-                    <div className="card-body">
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-header">Login</div>
+
+                        <div className="card-body">
                         <form>
-                            <div className="input-group form-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"><i className="fas fa-user"></i></span>
-                                </div>
-                                <input type="text" className="form-control" placeholder="username" />
-                                
-                            </div>
-                            <div className="input-group form-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"><i className="fas fa-key"></i></span>
-                                </div>
-                                <input type="password" className="form-control" placeholder="password" />
-                            </div>
-                            <div className="row align-items-center remember">
-                                <input type="checkbox" />Remember Me
-                            </div>
-                            <div className="form-group">
-                                <input type="submit" value="Login" className="btn btn-success float-right" />
-                            </div>
-                        </form>
-                    </div>
-                    <div className="card-footer">
-                        <div className="d-flex justify-content-center links">
-                            Don't have an account?<a href="#">Sign Up</a>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                         </div>
-                        <div className="d-flex justify-content-center">
-                            <a href="#">Forgot your password?</a>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" />
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                            <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
                         </div>
                     </div>
                 </div>
